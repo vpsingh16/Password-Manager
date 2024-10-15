052f1dac7bfbb70758d26a1a03e27d531b17c12c
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2024-10-15 11:10:20 +0530

    Update Manager.jsx

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -137,7 +137,7 @@ const Manager = () => {
                 <h1 className='text-4xl text font-bold text-center'>
                     <span className='text-green-500'> &lt;</span>
 
-                    <span>Pass</span><span className='text-green-500'>OP/&gt;</span>
+                    <span>Password</span><span className='text-green-500'>Manager/&gt;</span>
 
                 </h1>
                 <p className='text-green-900 text-lg text-center'>Your own Password Manager</p>
